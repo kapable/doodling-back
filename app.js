bd885f0e0dcd632e3908948ab3045d07eb7437bd
kapable <skjoldenstore@gmail.com>
2022-09-15 16:12:09 +0900

    fix docker file and compose file for volume mount path in dev mode

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Welcome to Doodling API!!!');
+    res.send('Welcome to Doodling API!');
 });
 
 app.listen(3065, () => {
-    console.log('Listening on port 3065');
+    console.log('Listening on http://localhost:3065');
 });
