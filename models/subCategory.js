f22facfef0eb89d0df990a4bbc740e74e92dc0a1
kapable <skjoldenstore@gmail.com>
2022-09-21 08:47:00 +0900

    refix file name

---
+++ models/subCategory.js
@@ -0,0 +1,28 @@
+const DataTypes = require('sequelize');
+const { Model } = DataTypes;
+
+module.exports = class SubCategory extends Model {
+    static init(sequelize) {
+        return super.init({
+            label: {
+                type: DataTypes.STRING(20),
+                allowNull: false,
+                unique: true,
+            },
+            enabled: {
+                type: DataTypes.BOOLEAN,
+                allowNull: false,
+            }
+        }, {
+            modelName: 'SubCategory',
+            tableName: 'subCategories',
+            charset: 'utf8',
+            collate: 'utf8_general_ci',
+            sequelize
+        });
+    };
+    static associate(db) {
+        db.SubCategory.belongsTo(db.Category);
+        db.SubCategory.hasMany(db.Post);
+    };
+};
