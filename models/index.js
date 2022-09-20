99811e2bb74cbc00373af31a2fc3da75506542b9
kapable <skjoldenstore@gmail.com>
2022-09-20 22:35:16 +0900

    test db conn

--- models/index.js
+++ models/index.js
@@ -1,10 +1,10 @@
 const ads = require('./ads');
-const category = require('./category');
-const subCategory = require('./subCategory');
-const comment = require('./comment');
-const image = require('./image');
-const post = require('./post');
-const user = require('./user');
+// const category = require('./category');
+// const subCategory = require('./subCategory');
+// const comment = require('./comment');
+// const image = require('./image');
+// const post = require('./post');
+// const user = require('./user');
 
 const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
@@ -14,12 +14,12 @@ const db = {};
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 db.Ads = ads;
-db.Category = category;
-db.SubCategory = subCategory;
-db.Comment = comment;
-db.Image = image;
-db.Post = post;
-db.User = user;
+// db.Category = category;
+// db.SubCategory = subCategory;
+// db.Comment = comment;
+// db.Image = image;
+// db.Post = post;
+// db.User = user;
 
 Object.keys(db).forEach(modelName => {
   db[modelName].init(sequelize);
